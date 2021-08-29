f7e83eb244ce581587d9d6c6b084916949bf1821
YueChao <yue327305210@163.com>
2021-08-29 10:59:20 +0800

    20210829 1100

--- spring-base/src/main/java/org/example/alvin/springexamples/bean/MyApplicationListener.java
+++ spring-base/src/main/java/org/example/alvin/springexamples/bean/MyApplicationListener.java
@@ -3,7 +3,9 @@ package org.example.alvin.springexamples.bean;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyApplicationListener implements ApplicationListener<MyApplicationEvent> {
 
   private static final Logger LOGGER = LogManager.getLogger(MyApplicationListener.class);
--- spring-base/src/test/java/org/example/alvin/springexamples/bean/TestBean.java
+++ spring-base/src/test/java/org/example/alvin/springexamples/bean/TestBean.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 class TestBean {
@@ -28,7 +29,12 @@ class TestBean {
   @Test
   void test3() {
     ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-    applicationContext.addApplicationListener(new MyApplicationListener());
+    /**
+     * 如果给MyApplicationListener加上@Component注解，则{@link MyApplicationListener#onApplicationEvent(MyApplicationEvent)}会被执行两次
+     * 观察者模式
+     * {@link ApplicationEventMulticaster#addApplicationListener(org.springframework.context.ApplicationListener)}
+     */
+//    applicationContext.addApplicationListener(new MyApplicationListener());
     applicationContext.publishEvent(new MyApplicationEvent("Alvin"));
     applicationContext.start();
   }
