c0bafe6053d5d011ca683c86bf4d4389bec2ea18
YueChao <yue327305210@163.com>
2021-10-03 11:00:48 +0800

    20211003 1100

--- spring-base/src/main/java/org/example/alvin/springexamples/annotation/aop/AspectAnnotation.java
+++ spring-base/src/main/java/org/example/alvin/springexamples/annotation/aop/AspectAnnotation.java
@@ -2,9 +2,14 @@ package org.example.alvin.springexamples.annotation.aop;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -19,9 +24,29 @@ public class AspectAnnotation {
 
   @Around("pc1()")
   private Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-    logger.info("====== AspectAnnotation around前置通知 ======");
+    logger.info("====== AspectAnnotation around 前置通知, 被增强方法: {} ======", joinPoint.getSignature().getName());
     Object result = joinPoint.proceed();
-    logger.info("====== AspectAnnotation around后置通知 ======");
+    logger.info("====== AspectAnnotation around 后置通知, 被增强方法: {} ======", joinPoint.getSignature().getName());
     return result;
+  }
+
+  @Before("pc1()")
+  private void before(JoinPoint joinPoint) {
+    logger.info("====== AspectAnnotation before 通知, 被增强方法: {} ======", joinPoint.getSignature().getName());
+  }
+
+  @After("pc1()")
+  private void after(JoinPoint joinPoint) {
+    logger.info("====== AspectAnnotation after 通知, 被增强方法: {} ======", joinPoint.getSignature().getName());
+  }
+
+  @AfterReturning("pc1()")
+  private void afterReturning(JoinPoint joinPoint) {
+    logger.info("====== AspectAnnotation afterReturning 通知, 被增强方法: {} ======", joinPoint.getSignature().getName());
+  }
+
+  @AfterThrowing("pc1()")
+  private void afterThrowing(JoinPoint joinPoint) {
+    logger.info("====== AspectAnnotation after afterThrowing, 被增强方法: {} ======", joinPoint.getSignature().getName());
   }
 }
