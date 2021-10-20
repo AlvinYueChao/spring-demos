3643c262e2f33acb13391488da712a7a0295c7da
YueChao <yue327305210@163.com>
2021-10-20 21:51:13 +0800

    20211020 2151

--- spring-base/src/main/java/org/example/alvin/springexamples/annotation/aop/transaction/DatabaseConfig.java
+++ spring-base/src/main/java/org/example/alvin/springexamples/annotation/aop/transaction/DatabaseConfig.java
@@ -1,7 +1,10 @@
 package org.example.alvin.springexamples.annotation.aop.transaction;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
 import javax.sql.DataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -13,6 +16,11 @@ import org.springframework.transaction.PlatformTransactionManager;
 @PropertySource(value = "application.properties")
 public class DatabaseConfig {
 
+  private final Logger logger = LogManager.getLogger(DatabaseConfig.class);
+
+  @Value("${datasource.driverClassName}")
+  private String driverClassName;
+
   @Value("${datasource.url}")
   private String url;
 
@@ -23,12 +31,27 @@ public class DatabaseConfig {
   private String password;
 
   @Bean
-  protected DataSource mysqlDataSource() {
-    MysqlDataSource mysqlDataSource = new MysqlDataSource();
-    mysqlDataSource.setURL(this.url);
-    mysqlDataSource.setUser(this.username);
-    mysqlDataSource.setPassword(this.password);
-    return mysqlDataSource;
+  protected DataSource comboPooledDataSource() {
+    ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+    try {
+      comboPooledDataSource.setDriverClass(driverClassName);
+      comboPooledDataSource.setJdbcUrl(url);
+      comboPooledDataSource.setUser(username);
+      comboPooledDataSource.setPassword(password);
+      comboPooledDataSource.setMinPoolSize(10);
+      comboPooledDataSource.setMaxPoolSize(100);
+      comboPooledDataSource.setAcquireIncrement(3);
+      comboPooledDataSource.setMaxStatements(1000);
+      comboPooledDataSource.setInitialPoolSize(10);
+      comboPooledDataSource.setIdleConnectionTestPeriod(60);
+      comboPooledDataSource.setAcquireRetryAttempts(30);
+      comboPooledDataSource.setBreakAfterAcquireFailure(false);
+      comboPooledDataSource.setTestConnectionOnCheckout(false);
+      comboPooledDataSource.setAcquireRetryDelay(100);
+    } catch (PropertyVetoException e) {
+      logger.error("Cannot find the available properties", e);
+    }
+    return comboPooledDataSource;
   }
 
   @Bean
