b1d8012d2babb03c14275528cd0e60d0b27790f3
YueChao <yue327305210@163.com>
2022-07-03 23:27:43 +0800

    20220703 2327

--- dubbo-p/src/main/java/dubbo/provider/xml/XmlProvider.java
+++ dubbo-p/src/main/java/dubbo/provider/xml/XmlProvider.java
@@ -1,5 +1,12 @@
 package dubbo.provider.xml;
 
+import java.io.IOException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class XmlProvider {
 
+  public static void main(String[] args) throws IOException {
+    new ClassPathXmlApplicationContext("applicationProvider.xml");
+    System.in.read();
+  }
 }
