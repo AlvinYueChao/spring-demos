ab6383dc73d3b528493821caf12ff84be7905993
YueChao <yue327305210@163.com>
2022-07-05 22:05:37 +0800

    20220705 2205

--- dubbo-c/src/test/java/dubbo/consumer/anno/AnnoBeanTest.java
+++ dubbo-c/src/test/java/dubbo/consumer/anno/AnnoBeanTest.java
@@ -1,5 +1,9 @@
 package dubbo.consumer.anno;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ConsumerConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.annotation.Reference;
 import dubbo.api.service.UserService;
 import java.io.IOException;
@@ -20,6 +24,25 @@ class AnnoBeanTest {
   @Test
   void test1() throws IOException {
     log.info("invoke result: {}", userService.queryUser("33"));
-    System.in.read();
+  }
+
+  @Test
+  void test2() {
+    ApplicationConfig applicationConfig = new ApplicationConfig();
+    applicationConfig.setName("dubbo_consumer");
+
+    ConsumerConfig consumerConfig = new ConsumerConfig();
+    consumerConfig.setCheck(false);
+
+    RegistryConfig registryConfig = new RegistryConfig();
+    registryConfig.setAddress("zookeeper://192.168.20.10:2181");
+
+    ReferenceConfig<UserService> referenceConfig = new ReferenceConfig<>();
+    referenceConfig.setApplication(applicationConfig);
+    referenceConfig.setConsumer(consumerConfig);
+    referenceConfig.setRegistry(registryConfig);
+    referenceConfig.setInterface(UserService.class);
+    UserService userService1 = referenceConfig.get();
+    log.info("manually invoke result: {}", userService1.queryUser("44"));
   }
 }
