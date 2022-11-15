00f6d1d00572241347b72ceaf1c0c523ce6389db
YueChao <yue327305210@163.com>
2022-11-15 21:22:06 +0800

    20221115 2122

--- spring-base/src/main/java/com/alvin/aspect/CustomAdvisors.java
+++ spring-base/src/main/java/com/alvin/aspect/CustomAdvisors.java
@@ -1,6 +1,8 @@
 package com.alvin.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,14 @@ public class CustomAdvisors {
 
   @Before("execution(public void com.alvin.service.UserService.test())")
   public void beforeAdvice() {
-    log.info("方法执行前...");
+    log.info("Before: 方法执行前...");
+  }
+
+  @Around("execution(public void com.alvin.service.UserService.test())")
+  public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
+    log.info("Around: 方法执行前");
+    Object result = pjp.proceed();
+    log.info("Around: 方法执行后");
+    return result;
   }
 }
