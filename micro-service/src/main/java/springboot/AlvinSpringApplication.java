82c1272a83bcee661942e17e05fc79a2a7c4ef92
YueChao <yue327305210@163.com>
2022-11-23 23:26:42 +0800

    20221123 2326

--- micro-service/src/main/java/springboot/AlvinSpringApplication.java
+++ micro-service/src/main/java/springboot/AlvinSpringApplication.java
@@ -1,5 +1,6 @@
 package springboot;
 
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Server;
@@ -17,6 +18,9 @@ import springboot.web.server.WebServer;
 @Slf4j
 public class AlvinSpringApplication {
 
+  private AlvinSpringApplication() {
+  }
+
   public static void run(Class<?> clazz) {
     AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
     applicationContext.register(clazz);
@@ -27,10 +31,22 @@ public class AlvinSpringApplication {
   }
 
   private static WebServer getWebServer(WebApplicationContext applicationContext) {
-    // 判断依赖的是Tomcat还是Netty
-    return applicationContext.getBean(WebServer.class);
+    // 判断依赖的是Tomcat还是Netty,在不加@Primary情况下，只能有一个WebServer类型的bean
+    Map<String, WebServer> webServers = applicationContext.getBeansOfType(WebServer.class);
+    if (webServers.isEmpty()) {
+      throw new NullPointerException();
+    } else if (webServers.size() > 1) {
+      throw new IllegalStateException();
+    } else {
+      return applicationContext.getBean(WebServer.class);
+    }
   }
 
+  /**
+   * @param applicationContext
+   * @deprecated hard code
+   */
+  @Deprecated(since = "20221123")
   private static void startTomcat(WebApplicationContext applicationContext) {
     Tomcat tomcat = new Tomcat();
 
