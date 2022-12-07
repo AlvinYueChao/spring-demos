c679542d47bbdd351ddcff8e1c5c3a44eebab35b
YueChao <yue327305210@163.com>
2022-12-07 22:45:57 +0800

    20221207 2245

--- micro-service/src/main/java/org/example/springboot/service/UserService.java
+++ micro-service/src/main/java/org/example/springboot/service/UserService.java
@@ -1,12 +1,23 @@
 package org.example.springboot.service;
 
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 public class UserService {
+
+  @Value("${key1}")
+  private String value;
+
+  @PostConstruct
+  public void init() {
+    // priority: command line args > system properties (VM options) > environment variables > application properties (application.yml)
+    log.info("value of key1: {}", value);
+  }
 
   public Mono<String> getUserById(long userId) {
     log.info("=== getUserById: {} ===", userId);
